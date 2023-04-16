4cc6065eed2e808bcf53bc365fefedffbd79deea
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2023-04-16 19:33:01 +0700

    Update Main.php

--- src/MulkiAqi192/TPBow/Main.php
+++ src/MulkiAqi192/TPBow/Main.php
@@ -8,7 +8,7 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
 	public function onEnable(): void {
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener());
 		$this->registerCommands();
 	}
 
